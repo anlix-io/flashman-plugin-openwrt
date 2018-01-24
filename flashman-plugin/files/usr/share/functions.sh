ceb3ad32e1bc35005a4e8423447f68d51420954b
Guilherme Senges <guisenges@gmail.com>
2018-01-24 14:52:11 -0200

    Convert indentation to two spaces

--- flashman-plugin/files/usr/share/functions.sh
+++ flashman-plugin/files/usr/share/functions.sh
@@ -4,44 +4,44 @@
 
 download_file()
 {
-    dfile="$2"
-    uri="$1/$dfile"
-    dest_dir="$3"
+  dfile="$2"
+  uri="$1/$dfile"
+  dest_dir="$3"
 
-    if [ "$#" -eq 3 ]
+  if [ "$#" -eq 3 ]
+  then
+    mkdir -p "$dest_dir"
+
+    if test -e "$dest_dir/$dfile"
     then
-        mkdir -p "$dest_dir"
-
-        if test -e "$dest_dir/$dfile"
-        then
-            zflag="-z $dest_dir/$dfile"
-        else
-            zflag=
-        fi
-
-        curl_code=`curl -s -k -w "%{http_code}" -u routersync:landufrj123 \
-                  --connect-timeout 5 --retry 3 \
-                  -o "/tmp/$dfile" $zflag "$uri"`
-
-        if [ "$curl_code" = "200" ]
-        then
-            mv "/tmp/$dfile" "$dest_dir/$dfile"
-            echo "Download file on $uri"
-            return 0
-        else
-            echo "File not downloaded on $uri"
-            if [ "$curl_code" != "304" ]
-            then
-                rm "/tmp/$dfile"
-                return 1
-            else
-                return 0
-            fi
-        fi
+      zflag="-z $dest_dir/$dfile"
     else
-        echo "Wrong number of arguments"
-        return 1
+      zflag=
     fi
+
+    curl_code=`curl -s -k -w "%{http_code}" -u routersync:landufrj123 \
+              --connect-timeout 5 --retry 3 \
+              -o "/tmp/$dfile" $zflag "$uri"`
+
+    if [ "$curl_code" = "200" ]
+    then
+      mv "/tmp/$dfile" "$dest_dir/$dfile"
+      echo "Download file on $uri"
+      return 0
+    else
+      echo "File not downloaded on $uri"
+      if [ "$curl_code" != "304" ]
+      then
+        rm "/tmp/$dfile"
+        return 1
+      else
+        return 0
+      fi
+    fi
+  else
+    echo "Wrong number of arguments"
+    return 1
+  fi
 }
 
 get_mac()
