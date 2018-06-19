a2873e2560a1fd621d0542a3c527c535faefaa4d
Guilherme Senges <guilherme@anlix.io>
2018-06-19 19:17:36 -0300

    Converted indentation from tabs to 2 spaces

--- flashman-plugin/files/usr/share/keepalive.sh
+++ flashman-plugin/files/usr/share/keepalive.sh
@@ -20,32 +20,32 @@ WIFI_CHANNEL=""
 _need_update=0
 while true
 do
-	sleep 300
+  sleep 300
 
-	_number=$(head /dev/urandom | tr -dc "012345" | head -c1)
+  _number=$(head /dev/urandom | tr -dc "012345" | head -c1)
 
-	if [ "$_number" -eq 3 ] || [ "$1" == "now" ]
-	then
-		# Get PPPoE data if available
-		if [ "$(uci get network.wan.proto)" == "pppoe" ]
-		then
-			PPPOE_USER=$(uci get network.wan.username)
-			PPPOE_PASSWD=$(uci get network.wan.password)
-		fi
+  if [ "$_number" -eq 3 ] || [ "$1" == "now" ]
+  then
+    # Get PPPoE data if available
+    if [ "$(uci get network.wan.proto)" == "pppoe" ]
+    then
+      PPPOE_USER=$(uci get network.wan.username)
+      PPPOE_PASSWD=$(uci get network.wan.password)
+    fi
 
-		# Get WiFi data if available
-		if [ "$(uci get wireless.@wifi-device[0].disabled)" == "0" ] || [ "$SYSTEM_MODEL" == "MT7628AN" ]
-		then
-			WIFI_SSID=$(uci get wireless.@wifi-iface[0].ssid)
-			WIFI_PASSWD=$(uci get wireless.@wifi-iface[0].key)
-			WIFI_CHANNEL=$(uci get wireless.radio0.channel)
-		fi
+    # Get WiFi data if available
+    if [ "$(uci get wireless.@wifi-device[0].disabled)" == "0" ] || [ "$SYSTEM_MODEL" == "MT7628AN" ]
+    then
+      WIFI_SSID=$(uci get wireless.@wifi-iface[0].ssid)
+      WIFI_PASSWD=$(uci get wireless.@wifi-iface[0].key)
+      WIFI_CHANNEL=$(uci get wireless.radio0.channel)
+    fi
 
      log "KEEPALIVE" "Ping Flashman ..."
-		_res=$(curl -s -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" \
-		         --tlsv1.2 --connect-timeout 5 --retry 0 \
-		         --data "id=$CLIENT_MAC&version=$OPENWRT_VER&model=$HARDWARE_MODEL&model_ver=$HARDWARE_VER&release_id=$FLM_RELID&pppoe_user=$PPPOE_USER&pppoe_password=$PPPOE_PASSWD&wan_ip=$WAN_IP_ADDR&wifi_ssid=$WIFI_SSID&wifi_password=$WIFI_PASSWD&wifi_channel=$WIFI_CHANNEL" \
-		         "https://$SERVER_ADDR/deviceinfo/syn/")
+    _res=$(curl -s -A "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1)" \
+           --tlsv1.2 --connect-timeout 5 --retry 0 \
+           --data "id=$CLIENT_MAC&version=$OPENWRT_VER&model=$HARDWARE_MODEL&model_ver=$HARDWARE_VER&release_id=$FLM_RELID&pppoe_user=$PPPOE_USER&pppoe_password=$PPPOE_PASSWD&wan_ip=$WAN_IP_ADDR&wifi_ssid=$WIFI_SSID&wifi_password=$WIFI_PASSWD&wifi_channel=$WIFI_CHANNEL" \
+           "https://$SERVER_ADDR/deviceinfo/syn/")
 
     json_load "$_res"
     json_get_var _do_update do_update
@@ -64,5 +64,5 @@ do
       log "KEEPALIVE" "Running update ..."                                                                                                                                          
       sh /usr/share/flashman_update.sh
     fi
-	fi
+  fi
 done
