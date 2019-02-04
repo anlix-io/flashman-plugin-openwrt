6b887ff5657baf4e25f70776446d7b253d8427ee
Guilherme Senges <guilherme@anlix.io>
2019-02-04 10:41:35 -0200

    Fixed led commands on 845n v4

--- custom-files/tl-wr845n-v4/usr/share/functions/device_functions.sh
+++ custom-files/tl-wr845n-v4/usr/share/functions/device_functions.sh
@@ -5,18 +5,6 @@ save_wifi_local_config() {
   /usr/bin/uci2dat -d radio0 -f /etc/wireless/mt7628/mt7628.dat > /dev/null
 }
 
-led_on() {
-  if [ -f "$1"/brightness ]
-  then
-    if [ -f "$1"/max_brightness ]
-    then
-      cat "$1"/max_brightness > "$1"/brightness
-    else
-      echo "255" > "$1"/brightness
-    fi
-  fi
-}
-
 led_off() {
   if [ -f "$1"/trigger ]
   then
@@ -25,49 +13,29 @@ led_off() {
   fi
 }
 
+led_netdev() {
+  echo "netdev" > "$1"/trigger
+  echo "link tx rx" > "$1"/mode
+  echo "$2" > "$1"/device_name
+}
+
 reset_leds() {
   for trigger_path in $(ls -d /sys/class/leds/*)
   do
     led_off "$trigger_path"
   done
-
   /etc/init.d/led restart > /dev/null
-
-  for system_led in /sys/class/leds/*system*
-  do
-    led_on "$system_led"
-  done
-
-  # reset hardware lan ports if any
-  for lan_led in /sys/class/leds/*lan*
-  do
-    if [ -f "$lan_led"/enable_hw_mode ]
-    then
-      echo 1 > "$lan_led"/enable_hw_mode
-    fi
-  done
-
-  # reset hardware wan port if any
-  for wan_led in /sys/class/leds/*wan*
-  do
-    if [ -f "$wan_led"/enable_hw_mode ]
-    then
-      echo 1 > "$wan_led"/enable_hw_mode
-    fi
-  done
+  led_netdev /sys/class/leds/$(cat /tmp/sysinfo/board_name)\:green\:wan eth0.2
 }
 
 blink_leds() {
   local _do_restart=$1
-
+  local _bname=$(cat /tmp/sysinfo/board_name)
   if [ $_do_restart -eq 0 ]
   then
-    ledsoff=$(ls -d /sys/class/leds/*)
-
-    for trigger_path in $ledsoff
-    do
-      echo "timer" > "$trigger_path"/trigger
-    done
+    led_off /sys/class/leds/$(cat /tmp/sysinfo/board_name)\:green\:wan
+    echo "timer" > /sys/class/leds/$_bname\:green\:wlan/trigger
+    echo "timer" > /sys/class/leds/$_bname\:orange\:wan/trigger
   fi
 }
 
