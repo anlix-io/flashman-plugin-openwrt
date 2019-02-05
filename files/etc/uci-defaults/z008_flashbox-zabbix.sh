b7f345dab0f96c9bf7d641d759c2684961d4125c
Guilherme Senges <guilherme@anlix.io>
2019-02-05 13:39:24 -0200

    Only creating zabbix PSK file if config JSON contains a PSK

--- files/etc/uci-defaults/z008_flashbox-zabbix.sh
+++ files/etc/uci-defaults/z008_flashbox-zabbix.sh
@@ -5,7 +5,10 @@
 json_cleanup
 json_load_file /root/flashbox_config.json
 json_get_var _zabbix_psk zabbix_psk
-echo -n $_zabbix_psk > /etc/zabbix_agentd.psk
 json_close_object
+if [ "$_zabbix_psk" != "" ]
+then
+  echo -n $_zabbix_psk > /etc/zabbix_agentd.psk
+fi
 
 exit 0
