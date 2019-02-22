c68dda67dbd3cc619be8feba13698aa987df3ad3
Thurler <gt@anlix.io>
2019-02-22 18:44:43 -0300

    Updated uci defaults scripts to properly initialize new zabbix parameters

--- files/etc/uci-defaults/z008_flashbox-zabbix.sh
+++ files/etc/uci-defaults/z008_flashbox-zabbix.sh
@@ -5,10 +5,16 @@
 json_cleanup
 json_load_file /root/flashbox_config.json
 json_get_var _zabbix_psk zabbix_psk
+json_get_var _zabbix_fqdn zabbix_fqdn
 json_close_object
 if [ "$_zabbix_psk" != "" ]
 then
   echo -n $_zabbix_psk > /etc/zabbix_agentd.psk
+fi
+if [ "$_zabbix_fqdn" != "" ]
+then
+  sed -i 's/Server=.*/Server='"$_zabbix_fqdn"'/' /etc/zabbix_agentd.conf
+  sed -i 's/ServerActive=.*/ServerActive='"$_zabbix_fqdn"':80/' /etc/zabbix_agentd.conf
 fi
 
 exit 0
