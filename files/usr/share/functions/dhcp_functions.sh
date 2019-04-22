e91fd80bea385f70330ca6d7114995105e52058d
Guilherme Senges <guilherme@anlix.io>
2019-04-22 18:50:50 -0300

    Improved default version of wifi stats with all parameters working

--- files/usr/share/functions/dhcp_functions.sh
+++ files/usr/share/functions/dhcp_functions.sh
@@ -17,7 +17,7 @@ get_device_conn_type() {
     # Wireless
     echo "1"
   else
-    local _state=$(arp -n | grep "$_mac" | awk '{ print $3 }')
+    local _state=$(arp -n | grep "$_mac" | awk '{print $3}')
     if [ "$_state" == "0x2" ]
     then
       # Wired
