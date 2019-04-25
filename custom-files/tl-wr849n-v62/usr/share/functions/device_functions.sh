b3657c8da5188bce657c112515ea6b0cef82b7b9
Guilherme Senges <guilherme@anlix.io>
2019-04-25 00:53:04 -0300

    Added wifi stats collection to mt728 targets

--- custom-files/tl-wr849n-v62/usr/share/functions/device_functions.sh
+++ custom-files/tl-wr849n-v62/usr/share/functions/device_functions.sh
@@ -11,13 +11,111 @@ is_5ghz_capable() {
 }
 
 get_wifi_device_stats() {
-  # TODO Implement this function
-  echo "0.0 0.0 0.0 0.0 0 Z"
+  local _dev_mac="$1"
+  local _dev_num
+  local _idx
+  local _dev_info=""
+  local _retstatus
+  local _cmd_res
+  local _wifi_itf="ra0"
+  local _wifi_stats=""
+  local _ap_freq="2.4"
+
+  _cmd_res=$(command -v iwpriv)
+  _retstatus=$?
+
+  if [ $_retstatus -eq 0 ]
+  then
+    _dev_num="$(iwpriv $_wifi_itf assoclist_num 2> /dev/null)"
+    _retstatus=$?
+    if [ $_retstatus -eq 0 ]
+    then
+      # Interface returned successfully
+      _dev_num="$(echo $_dev_num | awk -F: '{print $2}')"
+      for _idx in $(seq 1 $_dev_num)
+      do
+        iwpriv $_wifi_itf assoclist $_idx | grep $_dev_mac > /dev/null
+        _retstatus=$?
+        if [ $_retstatus -eq 0 ]
+        then
+          _dev_info="$(iwpriv $_wifi_itf assoclist $_idx)"
+          break
+        fi
+      done
+    fi
+
+    if [ "$_dev_info" != "" ]
+    then
+      local _dev_txbitrate="$(echo "$_dev_info" | grep 'tx bitrate:' | \
+                              awk '{print $3}')"
+      local _dev_rxbitrate="0.0"
+      local _dev_mode="$(echo "$_dev_info" | grep 'caps:' | \
+                         awk '{print $2}')"
+      local _dev_signal="$(echo "$_dev_info" | grep 'RSSI:' | \
+                           awk '{print $2}')"
+      local _dev_snr="$(echo "$_dev_info" | grep 'SNR:' | \
+                        awk '{print $2}')"
+
+      _wifi_stats="$_dev_txbitrate $_dev_rxbitrate $_dev_signal"
+      _wifi_stats="$_wifi_stats $_dev_snr $_ap_freq"
+
+      if [ "$_dev_mode" == "VHT" ]
+      then
+        # AC
+        _wifi_stats="$_wifi_stats AC"
+      elif [ "$_dev_mode" == "HT" ]
+      then
+        # N
+        _wifi_stats="$_wifi_stats N"
+      else
+        # G
+        _wifi_stats="$_wifi_stats G"
+      fi
+      echo "$_wifi_stats"
+    else
+      echo "0.0 0.0 0.0 0.0 0 Z"
+    fi
+  else
+    echo "0.0 0.0 0.0 0.0 0 Z"
+  fi
 }
 
 is_device_wireless() {
-  # TODO Implement this function
-  return 1
+  local _dev_mac="$1"
+  local _dev_num
+  local _idx
+  local _dev_info
+  local _retstatus
+  local _cmd_res
+  local _wifi_itf="ra0"
+
+  _cmd_res=$(command -v iwpriv)
+  _retstatus=$?
+
+  if [ $_retstatus -eq 0 ]
+  then
+    _dev_info="$(iwpriv $_wifi_itf assoclist_num 2> /dev/null)"
+    _retstatus=$?
+    if [ $_retstatus -eq 0 ]
+    then
+      # Interface returned successfully
+      _dev_num="$(echo $_dev_info | awk -F: '{print $2}')"
+      for _idx in $(seq 1 $_dev_num)
+      do
+        _dev_info="$(iwpriv $_wifi_itf assoclist $_idx | grep $_dev_mac)"
+        _retstatus=$?
+        if [ $_retstatus -eq 0 ]
+        then
+          return 0
+        fi
+      done
+    fi
+
+    # Not found
+    return 1
+  else
+    return 1
+  fi
 }
 
 led_power_off() {
