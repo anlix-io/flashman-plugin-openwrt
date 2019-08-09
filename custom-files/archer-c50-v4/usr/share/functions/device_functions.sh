2bbba66c32235f1f89372c954df44abd695f97c9
Guilherme Senges <guilherme@anlix.io>
2019-08-09 17:28:44 -0300

    Fixed .dat reload on wifi 5g

--- custom-files/archer-c50-v4/usr/share/functions/device_functions.sh
+++ custom-files/archer-c50-v4/usr/share/functions/device_functions.sh
@@ -3,7 +3,7 @@
 save_wifi_local_config() {
   uci commit wireless
   /usr/bin/uci2dat -d radio0 -f /etc/wireless/mt7628/mt7628.dat > /dev/null
-  /usr/bin/uci2dat -d radio1 -f /etc/Wireless/iNIC/iNIC_ap.dat > /dev/null
+  /usr/bin/uci2dat -d radio1 -f /etc/Wireless/mt7612/mt7612.dat > /dev/null
 }
 
 is_5ghz_capable() {
