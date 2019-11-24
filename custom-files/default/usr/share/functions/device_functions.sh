50e6e27c64b1a374e3bc1b935273bbc3ecf90bc5
Guilherme Senges <guilherme@anlix.io>
2019-11-24 01:14:06 -0300

    Added enable/disable wifi to targets that use atheros

--- custom-files/default/usr/share/functions/device_functions.sh
+++ custom-files/default/usr/share/functions/device_functions.sh
@@ -230,3 +230,52 @@ get_lan_dev_negotiated_speed() {
             awk -F: '{print $4}' | awk -F 'baseT' '{print $1}')"
   echo "$_speed"
 }
+
+store_enable_wifi() {
+  local _itf_num
+  # 0: 2.4GHz 1: 5.0GHz 2: Both
+  _itf_num=$1
+
+  wifi down
+
+  if [ "$_itf_num" = "0" ]
+  then
+    uci set wireless.@wifi-iface[0].disabled="0"
+  elif [ "$_itf_num" = "1" ]
+  then
+    uci set wireless.@wifi-iface[1].disabled="0"
+  else
+    uci set wireless.@wifi-iface[0].disabled="0"
+
+    if [ "$(uci -q get wireless.@wifi-iface[1])" ]
+    then
+      uci set wireless.@wifi-iface[1].disabled="0"
+    fi
+  fi
+  save_wifi_local_config
+  wifi up
+}
+
+store_disable_wifi() {
+  local _itf_num
+  # 0: 2.4GHz 1: 5.0GHz 2: Both
+  _itf_num=$1
+
+  wifi down
+
+  if [ "$_itf_num" = "0" ]
+  then
+    uci set wireless.@wifi-iface[0].disabled="1"
+  elif [ "$_itf_num" = "1" ]
+  then
+    uci set wireless.@wifi-iface[1].disabled="1"
+  else
+    uci set wireless.@wifi-iface[0].disabled="1"
+    if [ "$(uci -q get wireless.@wifi-iface[1])" ]
+    then
+      uci set wireless.@wifi-iface[1].disabled="1"
+    fi
+  fi
+  save_wifi_local_config
+  wifi up
+}
