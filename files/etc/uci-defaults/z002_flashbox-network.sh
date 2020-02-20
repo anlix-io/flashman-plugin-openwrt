9d4481b9122e1984df5e498ca7e3843888eb7ad2
Thurler <gt@anlix.io>
2020-02-20 14:52:10 -0300

    Fixed option name on z002 file

--- files/etc/uci-defaults/z002_flashbox-network.sh
+++ files/etc/uci-defaults/z002_flashbox-network.sh
@@ -9,7 +9,7 @@ _wan_proto_value=$(uci get network.wan.proto)
 json_cleanup
 json_load_file /root/flashbox_config.json
 json_get_var _bridge_mode bridge_mode
-json_get_var _bridge_switch bridge_switch
+json_get_var _bridge_disable_switch bridge_disable_switch
 json_get_var _bridge_fix_ip bridge_fix_ip
 json_get_var _bridge_fix_gateway bridge_fix_gateway
 json_get_var _bridge_fix_dns bridge_fix_dns
@@ -119,7 +119,7 @@ fi
 # Check if bridge mode should be enabled
 if [ "$_bridge_mode" = "y" ]
 then
-  enable_bridge_mode "$_bridge_switch" "$_bridge_fix_ip" "$_bridge_fix_gateway" "$_bridge_fix_dns"
+  enable_bridge_mode "$_bridge_disable_switch" "$_bridge_fix_ip" "$_bridge_fix_gateway" "$_bridge_fix_dns"
 fi
 
 exit 0
