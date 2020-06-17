d285e9c8129d5918148e36ddf90379d49bd03d54
Gaspare <gaspare@anlix.io>
2020-06-17 16:09:10 -0300

    Convert ident to tab in uci-default (save space in flash)

--- files/etc/uci-defaults/z002_flashbox-network.sh
+++ files/etc/uci-defaults/z002_flashbox-network.sh
@@ -26,13 +26,13 @@ json_close_object
 
 if [ "$_lan_addr" = "" ] || [ "$_lan_netmask" = "" ]
 then
-  _lan_addr="$FLM_LAN_SUBNET"
-  _lan_netmask="$FLM_LAN_NETMASK"
+	_lan_addr="$FLM_LAN_SUBNET"
+	_lan_netmask="$FLM_LAN_NETMASK"
 fi
 
 if [ "$_lan_ipv6prefix" = "" ]
 then
-  _lan_ipv6prefix="$FLM_LAN_IPV6_PREFIX"
+	_lan_ipv6prefix="$FLM_LAN_IPV6_PREFIX"
 fi
 
 # Validate LAN gateway address
@@ -40,26 +40,26 @@ valid_ip "$_lan_addr"
 _retstatus=$?
 if [ $_retstatus -eq 1 ]
 then
-  # Invalid. Use default 10.0.10.1
-  _lan_addr="10.0.10.1"
-  _lan_netmask="24"
+	# Invalid. Use default 10.0.10.1
+	_lan_addr="10.0.10.1"
+	_lan_netmask="24"
 else
-  _ipcalc_res="$(/bin/ipcalc.sh $_lan_addr $_lan_netmask 1)"
+	_ipcalc_res="$(/bin/ipcalc.sh $_lan_addr $_lan_netmask 1)"
 
-  _ipcalc_netmask=$(echo "$_ipcalc_res" | grep "PREFIX" | awk -F= '{print $2}')
-  # Accepted netmasks: 24 to 26
-  if [ $_ipcalc_netmask -lt 24 ] || [ $_ipcalc_netmask -gt 26 ]
-  then
-    # Invalid netmask. Use default 24
-    _lan_netmask="24"
-    _lan_addr="10.0.10.1"
-  else
-    # Valid netmask
-    _lan_netmask="$_ipcalc_netmask"
-    # Use first address available returned by ipcalc
-    _ipcalc_addr=$(echo "$_ipcalc_res" | grep "START" | awk -F= '{print $2}')
-    _lan_addr="$_ipcalc_addr"
-  fi
+	_ipcalc_netmask=$(echo "$_ipcalc_res" | grep "PREFIX" | awk -F= '{print $2}')
+	# Accepted netmasks: 24 to 26
+	if [ $_ipcalc_netmask -lt 24 ] || [ $_ipcalc_netmask -gt 26 ]
+	then
+		# Invalid netmask. Use default 24
+		_lan_netmask="24"
+		_lan_addr="10.0.10.1"
+	else
+		# Valid netmask
+		_lan_netmask="$_ipcalc_netmask"
+		# Use first address available returned by ipcalc
+		_ipcalc_addr=$(echo "$_ipcalc_res" | grep "START" | awk -F= '{print $2}')
+		_lan_addr="$_ipcalc_addr"
+	fi
 fi
 
 # Configure WAN
@@ -85,29 +85,29 @@ uci set network.dmz.ipv6='0'
 # Check custom wan type
 if [ "$_wan_conn_type" = "pppoe" ] || [ "$_wan_conn_type" = "dhcp" ]
 then
-  uci set network.wan.proto="$_wan_conn_type"
+	uci set network.wan.proto="$_wan_conn_type"
 fi
 
 if { [ "$_wan_conn_type" = "" ] && [ "$FLM_WAN_PROTO" = "pppoe" ]; } || \
-   [ "$_wan_conn_type" = "pppoe" ];
+	 [ "$_wan_conn_type" = "pppoe" ];
 then
-  uci set network.wan.username="$FLM_WAN_PPPOE_USER"
-  uci set network.wan.password="$FLM_WAN_PPPOE_PASSWD"
+	uci set network.wan.username="$FLM_WAN_PPPOE_USER"
+	uci set network.wan.password="$FLM_WAN_PPPOE_PASSWD"
 fi
 # Check for custom pppoe credentials
 if [ "$_wan_conn_type" = "pppoe" ] && \
-   [ "$_pppoe_user" != "" ] && [ "$_pppoe_pass" != "" ]
+	 [ "$_pppoe_user" != "" ] && [ "$_pppoe_pass" != "" ]
 then
-  uci set network.wan.username="$_pppoe_user"
-  uci set network.wan.password="$_pppoe_pass"
+	uci set network.wan.username="$_pppoe_user"
+	uci set network.wan.password="$_pppoe_pass"
 fi
 # Check if IPv6 enabled
 if [ "$FLM_WAN_IPV6_ENABLED" == "y" ]
 then
-  uci set network.wan.ipv6="auto"
+	uci set network.wan.ipv6="auto"
 else
-  uci set network.wan.ipv6="0"
-  uci set network.lan.ipv6="0"
+	uci set network.wan.ipv6="0"
+	uci set network.lan.ipv6="0"
 fi
 
 
@@ -119,25 +119,25 @@ uci commit network
 A=$(grep "$_lan_addr anlixrouter" /etc/hosts)
 if [ ! "$A" ]
 then
-  echo "$_lan_addr anlixrouter" >> /etc/hosts
+	echo "$_lan_addr anlixrouter" >> /etc/hosts
 fi
 
 # Check if bridge mode should be enabled
 if [ "$_bridge_mode" = "y" ]
 then
-  enable_bridge_mode "n" "n" "$_bridge_disable_switch" "$_bridge_fix_ip" \
-                     "$_bridge_fix_gateway" "$_bridge_fix_dns"
+	enable_bridge_mode "n" "n" "$_bridge_disable_switch" "$_bridge_fix_ip" \
+			"$_bridge_fix_gateway" "$_bridge_fix_dns"
 fi
 
 if [ "$_mesh_mode" -gt "0" ]
 then
-  if [ -z "$_mesh_master" ]
-  then
-    set_mesh_master_mode "$_mesh_mode"
-  else
-    set_mesh_slave_mode "$_mesh_mode" "$_mesh_master"
-  fi
-  enable_mesh_routing "$_mesh_mode"
+	if [ -z "$_mesh_master" ]
+	then
+		set_mesh_master_mode "$_mesh_mode"
+	else
+		set_mesh_slave_mode "$_mesh_mode" "$_mesh_master"
+	fi
+	enable_mesh_routing "$_mesh_mode"
 fi
 
 exit 0
