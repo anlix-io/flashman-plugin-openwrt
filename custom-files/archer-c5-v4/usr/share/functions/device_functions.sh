58936984162d49270138b73b90d69f4f7401bcce
Guilherme Senges <guilherme@anlix.io>
2020-06-25 17:10:45 -0300

    Added fix to bridge mode in Archer C5 v4 target

--- custom-files/archer-c5-v4/usr/share/functions/device_functions.sh
+++ custom-files/archer-c5-v4/usr/share/functions/device_functions.sh
@@ -365,3 +365,31 @@ get_wifi_state() {
     fi
   fi
 }
+
+# Enable/disable ethernet connection on LAN physical ports when in bridge mode
+set_switch_bridge_mode() {
+  local _disable_lan_ports="$1"
+
+  if [ "$_disable_lan_ports" = "y" ]
+  then
+    # eth0
+    swconfig dev switch1 vlan 2 set ports ''
+    # eth1
+    swconfig dev switch1 vlan 1 set ports '4 5t'
+  else
+    # eth0.2
+    swconfig dev switch1 vlan 2 set ports ''
+    # eth0.1
+    swconfig dev switch1 vlan 1 set ports '0 1 2 3 4 5t'
+  fi
+}
+
+# Needs reboot to validate switch config   
+needs_reboot_bridge_mode() {
+  reboot
+}
+
+keep_ifnames_in_bridge() {
+  # true
+  echo "1"
+}
