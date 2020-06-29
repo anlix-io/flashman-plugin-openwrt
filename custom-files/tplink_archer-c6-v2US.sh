2c1c540c248fc07c12bbfa9496705569462f6237
Guilherme Senges <guilherme@anlix.io>
2020-06-29 14:23:13 -0300

    Added remaining gigabit models to use same bridge procedure of Archer C5

--- custom-files/tplink_archer-c6-v2US.sh
+++ custom-files/tplink_archer-c6-v2US.sh
@@ -7,3 +7,29 @@ get_custom_hardware_model() {
 get_custom_hardware_version() {
 	echo "V2US"
 }
+
+# Enable/disable ethernet connection on LAN physical ports when in bridge mode
+set_switch_bridge_mode() {
+  local _enable_bridge="$1"
+  local _disable_lan_ports="$2"
+
+  if [ "$_enable_bridge" = "y" ]
+  then
+    if [ "$_disable_lan_ports" = "y" ]
+    then
+      uci set network.@switch_vlan[0].ports='1 0t'
+      uci set network.@switch_vlan[1].ports=''
+    else
+      uci set network.@switch_vlan[0].ports='1 2 0t'
+      uci set network.@switch_vlan[1].ports=''
+    fi
+  else
+    uci set network.@switch_vlan[0].ports='2 0t'
+    uci set network.@switch_vlan[1].ports='1 0t'
+  fi
+}
+
+# Will not change ifnames if this variable is set when in bridge mode
+keep_ifnames_in_bridge_mode() {
+  echo "1"
+}
