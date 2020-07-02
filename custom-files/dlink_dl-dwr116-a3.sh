26c60377170156c971b6ea0fdd62b438e7cb5e5e
Guilherme Senges <guilherme@anlix.io>
2020-07-02 02:16:29 -0300

    Fixed wrong function name

--- custom-files/dlink_dl-dwr116-a3.sh
+++ custom-files/dlink_dl-dwr116-a3.sh
@@ -8,7 +8,7 @@ get_custom_hardware_version() {
 	echo "A3"
 }
 
-get_mac() {
+get_custom_mac() {
   local _mac_address_tag=""
   local _p0
   _p0=$(awk '{print toupper($1)}' /sys/class/ieee80211/phy0/macaddress)
