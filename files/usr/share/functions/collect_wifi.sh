24adb0920908919632f7248b38847901908c21fe
cassiohg <cassiohg@gmail.com>
2020-07-02 14:26:55 -0300

    finished collecting wan statistic

--- files/usr/share/functions/collect_wifi.sh
+++ files/usr/share/functions/collect_wifi.sh
@@ -2,9 +2,9 @@
 . /usr/share/functions/device_functions.sh
 . /usr/share/flashman_init.conf
 
-collectedDataPath="/tmp/influx_line_protocol"
-wifiDataPath=$collectedDataPath"/wifi"
-influxCompressedDataPath=$collectedDataPath"/compressed"
+influxDataDir="/tmp/influx_data_collecting"
+influxRawDataDir=$influxDataDir"/raw"
+influxCompressedDataDir=$influxDataDir"/compressed"
 # $influxDBAddress is inside /usr/share/flashman_init.conf, it holds the ip to influxdb.
 
 # prints the name of all available wireless interfaces, separated by new lines.
@@ -81,15 +81,14 @@ removeOldFiles() {
 }
 
 
-# extract data from devices in wifi and append to a new file created at given
-# $dataPath ($1) which name is the current timestamp.
+# extract data from devices in wifi, builds a string in influx line protocol 
+# format, which uses the router's mac address ($2) to identify the router 
+# each device belongs to, and append that string to file given as first 
+# argument ($1).
 collectDevicesWifiDataForInflux() {
-	local dataPath="$1"
-	mkdir -p "$dataPath" # if 'dataPath' doesn't exists, create it and all parent directories.
+	local filename="$1" mac="$2"
 
-	local timestamp=$(date +%s) # gets current unix time in seconds.
-	local newFilename="${dataPath}/${timestamp}" # name of the filed being created holding current collected data.
-	local mac=$(get_mac); # defined in /usr/share/functions/device_functions.sh
+	local timestamp=$(date +%s) # getting current unix time in seconds.
 	local allDevices oneDevice snr
 
 	for interface in $(getAllInterfaceNames); do # for each interface in the router;
@@ -106,62 +105,100 @@ collectDevicesWifiDataForInflux() {
 			# following command extract fields from one device and builds a 
 			# string as influx line protocol format and append that to a file.
 			echo $oneDevice | sed "
-			s/\([0-9a-f:]*\).* rx bytes: \([0-9]*\).*rx packets: \([0-9]*\).*tx bytes: \([0-9]*\).*tx packets: \([0-9]*\).*signal:\s*\([-0-9]*\)\s.*dBm.*tx bitrate: \([0-9]*\.[0-9]*\).*rx bitrate: \([0-9]*\.[0-9]*\).*/wifi,d=\1,r=${mac} ib=\2i,ip=\3i,ob=\4i,op=\5i,rx=\8,sig=\6i,snr=${snr},tx=\7 ${timestamp}/
-			s/://g" >> "$newFilename"; # file name is the timestamp, this helps to order the files by creation time.
+			s/\([0-9a-f:]*\).* rx bytes: \([0-9]*\).*rx packets: \([0-9]*\).*tx bytes: \([0-9]*\).*tx packets: \([0-9]*\).*signal:\s*\([-0-9]*\)\s.*dBm.*tx bitrate: \([0-9]*\.[0-9]*\).*rx bitrate: \([0-9]*\.[0-9]*\).*/wifi,d=\1,r=${mac} rx=\8,rxb=\2i,rxp=\3i,sig=\6i,snr=${snr},tx=\7,txb=\4i,txp=\5i ${timestamp}/
+			s/://g" >> "$filename"; # file name is the timestamp, this helps to order the files by creation time.
 		done
 	done
 }
 
-collectPingForInflux() {
-	local dataPath="$1"
-	mkdir -p "$dataPath" # if 'dataPath' doesn't exists, create it and all parent directories.
+# gets wan info from /sys/class/net/$wanName/statistics/ and builds a string 
+# in influx line protocol format, which uses the router's mac address, given 
+# as second argument ($2), to identify the router, and append that string to 
+# file given as first argument ($1).
+collectWanStatisticsForInflux() {
+	local filename="$1" mac="$2"
 
-	local timestamp=$(date +%s) # gets current unix time in seconds.
-	local newFilename="${dataPath}/${timestamp}" # name of the filed being created holding current collected data.
-	local mac=$(get_mac); # defined in /usr/share/functions/device_functions.sh
-	mac=${mac//:/}
-
-	local string="ping,r=$mac "
-	local pingNumber pingTime unreachable
-
-
-	local pingResult=$(ping -i 0.01 -c 100 "$FLM_SVADDR")
-	local pingSummary=${pingResult##* ---}
-	string=$(printf "%s" "$pingResult" | head -n -4 | sed '1d' | (while read line; do
-		unreachable=${line% Unreachable}
-		[ ${#unreachable} -eq ${#line} ] || continue
-
-		pingNumber=${line#*icmp_req=}
-		pingTime=${line#*time=}
-		string="${string}${pingNumber%% *}=${pingTime%% *},"
-	done
-	echo $string))
-
-	local loss=${pingSummary%\% packet loss*}
-	loss=${loss##* }
-	if [ $loss -ne 100 ]; then
-		local average=${pingSummary#*[0-9]/}
-		string="${string}avg=${average%%/*},"
-	fi
-	string="${string}loss=${loss}i ${timestamp}"
-
-	printf "string is: '%s'\n" "$string"
-
-	# echo "$string" > "$newFilename"
+	local timestamp=$(date +%s) # getting current unix time in seconds.
+	local wanName=$(uci get network.wan.ifname) # name of the wan interface.
+	local rxBytes=$(cat /sys/class/net/$wanName/statistics/rx_bytes)
+	local txBytes=$(cat /sys/class/net/$wanName/statistics/tx_bytes)
+	local rxPackets=$(cat /sys/class/net/$wanName/statistics/rx_packets)
+	local txPackets=$(cat /sys/class/net/$wanName/statistics/tx_packets)
+	local string="wan,r=$mac rxb=${rxBytes}i,rxp=${rxPackets}i,txb=${txBytes}i,txp=${txPackets}i $timestamp"
+	# echo s$string
+	echo "$string" > "$filename"
 }
 
-# collect data and store in a path, if the size of the files is this path is 
-# too big, join all files in a new file in a path for compressed files and 
-# compress it.
-collectDevicesWifiData() {
-	collectDevicesWifiDataForInflux "$wifiDataPath"
+# executes 100 pings, in burst, to flashman server, extracts the individual 
+# icmp request number and its respective ping time and builds a string in
+# influx line protocol format, which uses the router's mac address, given 
+# as second argument ($2), to identify the router, and append that string 
+# to file given as first argument ($1).
+collectPingForInflux() {
+	local filename="$1" mac="$2"
+
+	local timestamp=$(date +%s) # getting current unix time in seconds.
+	local string="ping,r=$mac " # beginning of influx line protocol string.
+	local pingNumber pingTime unreachable
+
+	local pingResult=$(ping -i 0.01 -c 100 "$FLM_SVADDR") # 100 pings in burst.
+	local pingSummary=${pingResult##* ---} # the summary that appears in the last lines.
+
+	# as numerical characters are small than letters, the icmp request numbers 
+	# are written first to string, then the letters charactered fields are 
+	# written.
+
+	# removing the first line and the last 4 lines. only the ping lines remain.
+	string=$(printf "%s" "$pingResult" | head -n -4 | sed '1d' | (while read line; do # for each ping line.
+		unreachable=${line% Unreachable} # removes ' Unreachable' part if it exists.
+		# if " Unreachable" has actually been removed, it means that line 
+		# contains it, which also means that icmp request wasn't fulfilled. 
+		[ ${#unreachable} -eq ${#line} ] || continue # if line contains ' Unreachable', skip this line.
+
+		pingNumber=${line#*icmp_req=} # from the whole line, removes everything until, and including, "icmp_req=".
+		pingNumber=${pingNumber%% *} # removes everything after the first space.
+		pingTime=${line#*time=} # from the whole line, removes everything until, and including, "time=".
+		pingTime=${pingTime%% *} # removes everything after the first space.
+		string="${string}$pingNumber=$pingTime," # concatenate to $string.
+	done
+	echo $string)) # prints final $string in this sub shell back to $string.
+
+	local loss=${pingSummary%\% packet loss*} # removes everything after, and including, '% packet loss'.
+	loss=${loss##* } # removes everything after first space.
+	if [ $loss -ne 100 ]; then # if $loss is not '100'.
+		local average=${pingSummary#*[0-9]/} # takes average after removing everything until, and including first digit followed by '/'.
+		average=${average%%/*} # removes everything after first '/'.
+		string="${string}avg=$average," # append average information.
+	fi # when loss is 100%, no rtt information is present in ping result, which means there is no average, min or max info.
+	string="${string}loss=${loss}i ${timestamp}" # append loss information and $timestamp to string.
+
+	# printf "string is: '%s'\n" "$string"
+	echo "$string" > "$filename" # appending string to file.
+}
+
+# collect every data and stores in a file which name is the time when the 
+# script ran, given as first argument ($1), in a directory for uncompressed 
+# files. if the size of the files is this directory is too big, join all files 
+# in a new file in a directory for compressed files and compress it.
+collectData() {
+	local time="$1"
+
+	# getting routers mac address used to identify the router.
+	local mac=$(get_mac); # defined in /usr/share/functions/device_functions.sh
+	mac=${mac//:/} # removing all colons in mac address.
+	mkdir -p "$influxRawDataDir" # if 'dataPath' doesn't exists, create it and all parent directories.
+	local file="${influxRawDataDir}/$time"
+
+	collectDevicesWifiDataForInflux "$file" "$mac"
+	collectWanStatisticsForInflux "$file" "$mac"
+	collectPingForInflux "$file" "$mac"
 
 	# $(zipFiles) returns 0 only if any amount of files has been compressed 
 	# and, consequently, moved to the directory of compressed files. So
-	# $(removeOldFiles) is only execute if any a new compressed file was 
+	# $(removeOldFiles) is only execute if any new compressed file was 
 	# created.
-	zipFiles "$wifiDataPath/*" 6144 "$influxCompressedDataPath/wifi_"$(date +"%FT%H-%M-%S") && \
-	removeOldFiles "$influxCompressedDataPath/*" 2048
+	zipFiles "$influxRawDataDir/*" 6144 "$influxCompressedDataDir/wifi_"$(date +"%FT%H-%M-%S") && \
+	removeOldFiles "$influxCompressedDataDir/*" 2048
 }
 
 
@@ -241,18 +278,18 @@ sendCompressedData() {
 	done
 }
 
-# for each non compressed file given in first argument ($1), append their 
+# for each uncompressed file given in first argument ($1), append their 
 # content to a new file at path given in second argument ($2). Then, compress 
 # it and send it to a server at address given in third argument ($3) and 
 # delete it. If send was successful, remove original files, if not, keep them. 
 # Returns the return of $(curl).
-sendNonCompressedData() {
-	local nonCompressedFilesPaths="$1" tempFilePath="$2" serverAddress="$3"
+sendUncompressedData() {
+	local uncompressedFilesPaths="$1" tempFilePath="$2" serverAddress="$3"
 
-	# echo going to send non compressed files
+	# echo going to send uncompressed files
 	
 	mkdir -p ${tempFilePath%/*} # mkdir with the directory the temp file should be in.
-	for i in $nonCompressedFilesPaths; do # for each non compressed file in the pattern expansion.
+	for i in $uncompressedFilesPaths; do # for each uncompressed file in the pattern expansion.
 		[ -f "$i" ] || continue # check if file still exists.
 		# echo appending $i to final file
 		cat $i >> "$tempFilePath" # append it's content to the temporary file.
@@ -270,8 +307,8 @@ sendNonCompressedData() {
 	# echo removed compressed final file
 
 	if [ "$sentResult" -eq 0 ]; then # if send was successful
-		# echo removing non compressed files
-		rm $nonCompressedFilesPaths # remove original files.
+		# echo removing uncompressed files
+		rm $uncompressedFilesPaths # remove original files.
 	fi
 	return $sentResult # Returns #(curl) exit code.
 }
@@ -312,32 +349,32 @@ random1To9() {
 }
 
 # check if it's time to send data, check if last time that data was sent the 
-# server was alive and then send all compressed files and all non compressed 
+# server was alive and then send all compressed files and all uncompressed 
 # files. if everything went ok, it will only send data again after as many 
 # calls, to this function, as $defaultBackoff.
-sendDevicesWifiData() {
+sendData() {
 	local defaultBackOff=5 # amount of calls needed to trigger the sending of data.
 
 	# check if we will send data this time and returns 0 if true. this controls if we will send data at this call.
-	checkBackoff "${collectedDataPath}/backoffCounter" $defaultBackOff
+	checkBackoff "${influxDataDir}/backoffCounter" $defaultBackOff
 	if [ "$?" -eq 0 ]; then # if $(checkBackoff) returned 0.
 
 		# check if the last time, data was sent, server was alive. if it was, 
-		# send compressed data, if everything was sent, send non compressed 
+		# send compressed data, if everything was sent, send uncompressed 
 		# data. If server wasn't alive last time, ping it and if it's alive 
 		# now, then send all data, but if it's still dead, do nothing.
-		local lastServerState=$(getLastServerState "$collectedDataPath/serverState")
+		local lastServerState=$(getLastServerState "$influxDataDir/serverState")
 		checkLastServerState "$lastServerState" $influxDBAddress && \
-		sendCompressedData "${influxCompressedDataPath}/*.gz" $influxDBAddress && \
-		sendNonCompressedData "${wifiDataPath}/*" "${collectedDataPath}/latestdata" $influxDBAddress
+		sendCompressedData "${influxCompressedDataDir}/*.gz" $influxDBAddress && \
+		sendUncompressedData "${influxRawDataDir}/*" "${influxDataDir}/latestdata" $influxDBAddress
 		local currentServerState="$?"
 		# if server stops before sending some data, current server state will differ from last server state.
 		# $currentServerState get the exit code of the first of these 3 functions above that returns anything other than 0.
 
 		# writes the $(curl) exit code if has changed since last attempt to send data.
-		writeCurrentServerState $currentServerState $lastServerState "$collectedDataPath/serverState"
+		writeCurrentServerState $currentServerState $lastServerState "$influxDataDir/serverState"
 		# sets a random backoff if $(curl) exit code wasn't equal 0.
-		writeBackoff $currentServerState $defaultBackOff $((1 + $(random1To9))) "${collectedDataPath}/backoffCounter"
+		writeBackoff $currentServerState $defaultBackOff $((1 + $(random1To9))) "${influxDataDir}/backoffCounter"
 	fi
 }
 
@@ -376,12 +413,12 @@ getStartTime() {
 
 loop() {
 	local interval=5 # interval between beginnings of data collecting.
-	local time=$(getStartTime "$collectedDataPath/startTime" $interval) # time when we will start executing.
+	local time=$(getStartTime "$influxDataDir/startTime" $interval) # time when we will start executing.
 	while true; do # infinite loop where we execute all procedures over and over again until the end of times.
 		echo startTime $time
 
-		collectDevicesWifiData # does everything related to collecting and storing data.
-		sendDevicesWifiData # does everything related to sending data and deletes data sent.
+		collectData "$time" # does everything related to collecting and storing data.
+		sendData # does everything related to sending data and deletes data sent.
 
 		local endTime=$(date +%s) # time after all procedures are finished.
 		local timeLeftForNextRun="-1" # this will hold the time left until we should run the next iteration of this loop
