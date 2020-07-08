ff684107ba90ef6db7b52173bb8756a55662e81e
Gaspare <gaspare@anlix.io>
2020-07-08 16:40:44 -0300

    Use informed mac address in dhcp requests for ANLIX routers to register in SAPO

--- files/etc/hotplug.d/dhcp/99-anlix-fingerprint.sh
+++ files/etc/hotplug.d/dhcp/99-anlix-fingerprint.sh
@@ -10,9 +10,10 @@ then
   if [ "${DNSMASQ_VENDOR_CLASS:0:5}" == "ANLIX" ]
   then
     . /usr/share/libubox/jshn.sh
+    _mac="$(echo $DNSMASQ_SUPPLIED_HOSTNAME | sed 's/-/:/g')"
     json_cleanup
     json_init
-    json_add_string mac "$MACADDR"
+    json_add_string mac "$_mac"
     [ "${DNSMASQ_VENDOR_CLASS#ANLIX}" == "02" ] && json_add_int status 0 || json_add_int status 1
     ubus call anlix_sapo notify_sapo "$(json_dump)"
     json_close_object
