1725761626c711227588f67567bb0ef5ce088752
cassiohg <cassiohg@gmail.com>
2020-08-04 23:20:40 -0300

    not writing "data_collecting_is_active" to "/root/flashbox_config.json" as it's redundant.

--- files/usr/share/functions/data_collecting_functions.sh
+++ files/usr/share/functions/data_collecting_functions.sh
@@ -8,22 +8,15 @@ data_collecting_service() {
 set_data_collecting_parameters() {
 	local data_collecting_fqdn="$1" data_collecting_is_active="$2"
 	
-	local saved_data_collecting_fqdn saved_data_collecting_is_active
+	local saved_data_collecting_fqdn
 	json_cleanup
 	json_load_file /root/flashbox_config.json
 	json_get_var saved_data_collecting_fqdn data_collecting_fqdn
-	json_get_var saved_data_collecting_is_active data_collecting_is_active
 
 	# Check if $data_collecting_fqdn has changed.
 	if [ "$saved_data_collecting_fqdn" != "$data_collecting_fqdn" ]; then
 		log "DATA COLLECTING" "Updating data_collecting_fqdn parameter"
 		json_add_string data_collecting_fqdn "$data_collecting_fqdn"
-	fi
-
-	# Check if $data_collecting_is_active has changed.
-	if [ "$saved_data_collecting_is_active" != "$data_collecting_is_active" ]; then
-		log "DATA COLLECTING" "Updating data_collecting_is_active parameter"
-		json_add_string data_collecting_is_active "$data_collecting_is_active"
 	fi
 
 	json_dump > /root/flashbox_config.json
@@ -43,24 +36,11 @@ set_data_collecting_parameters() {
 }
 
 set_data_collecting_on_off() {
-	# jshn translates false values to string "0". to simplify our lives, 
-	# we write string "0" when we mean boolean false.
-	local is_active="0" 
-
 	if [ "$1" = "on" ]; then
 		log "DATA COLLECTING" "Starting data collecting service"
 		data_collecting_service start
-		# jshn translates true boolean values to string "1", to simplify our 
-		# lives, we write string "1" when we mean boolean true.
-		is_active="1"
 	elif [ "$1" = "off" ]; then
 		log "DATA COLLECTING" "Stopping data collecting service"
 		data_collecting_service stop
 	fi
-
-	json_cleanup
-	json_load_file /root/flashbox_config.json
-	json_add_string data_collecting_is_active "$is_active"
-	json_dump > /root/flashbox_config.json
-	json_close_object
 }
