88c7187ed7c9e3a2a672f96d319e484a5f458aa9
Guilherme Senges <guilherme@anlix.io>
2020-08-31 15:01:30 -0300

    Testing hostapd_cli trigger by hotplug action

--- files/usr/share/hostapdstats.sh
+++ files/usr/share/hostapdstats.sh
@@ -1,3 +1,24 @@
 #!/bin/sh
 
-logger -t $0 "hostapd event received $1 $2 $3"
+WLAN_ITF="$1"
+EVENT="$2"
+INFO="$3"
+
+# WPS event handlers
+if [ "$EVENT" == "WPS-PBC-ACTIVE" ]
+then
+	# Push button active
+	logger -t $0 "Push button active"
+elif [ "$EVENT" == "WPS-TIMEOUT" ] || [ "$EVENT" == "WPS-PBC-DISABLE" ]
+then
+	# Push button disabled
+	logger -t $0 "Push button disabled"
+elif [ "$EVENT" == "WPS-SUCCESS" ]
+then
+	# Auth success
+	logger -t $0 "WPS auth successfull"
+elif [ "$EVENT" == "WPS-REG-SUCCESS" ]
+then
+	# Device MAC authenticated by WPS
+	logger -t $0 "Device connected $INFO"
+fi
