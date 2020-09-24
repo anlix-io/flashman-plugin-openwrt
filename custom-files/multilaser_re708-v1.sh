f9a32afe8eedcf1d8c10c1d89e4260d05839c9b3
Gaspare <gaspare@anlix.io>
2020-09-23 21:27:33 -0300

    Adjust mac addres of realteks

--- custom-files/multilaser_re708-v1.sh
+++ custom-files/multilaser_re708-v1.sh
@@ -23,7 +23,7 @@ get_custom_mac() {
 	local _mac_address_tag=""
 	local _p1
 
-	_p1=$(mtd_get_mac_binary boot 131079 | awk '{print toupper($1)}')
+	_p1=$(mtd_get_mac_binary config 7 | awk '{print toupper($1)}')
 	[ ! -z "$_p1" ] && _mac_address_tag=$_p1
 
 	echo "$_mac_address_tag"
