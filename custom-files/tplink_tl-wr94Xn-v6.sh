8346272d8239f5bcc2aa5c99da664625ca4c33eb
Guilherme Senges <guilherme@anlix.io>
2020-12-11 19:11:16 -0300

    Removed unnecessary wireless driver down in 94X v6 target. Causing crash

--- custom-files/tplink_tl-wr94Xn-v6.sh
+++ custom-files/tplink_tl-wr94Xn-v6.sh
@@ -9,34 +9,13 @@ anlix_force_clean_memory() {
 	echo 3 > /proc/sys/vm/drop_caches
 }
 
-anlix_upgrade_clean_memory() {
-	wifi down
-	sleep 3
-	rmmod ath9k
-	rmmod ath9k_common
-	rmmod ath9k_hw
-	rmmod mac80211
-	echo 3 > /proc/sys/vm/drop_caches
-}
-
-anlix_upgrade_restore_memory() {
-	modprobe mac80211
-	modprobe ath9k_hw
-	modprobe ath9k_common
-	modprobe ath9k
-	wifi up
-}
-
 get_custom_mac() {
 	local _mac_address_tag=""
 	local _p0
-	_p0=$(awk '{print toupper($1)}' /sys/class/ieee80211/phy0/macaddress 2> /dev/null)
+	_p0=$(awk '{print toupper($1)}' /sys/class/ieee80211/phy0/macaddress)
 
 	if [ ! -z "$_p0" ]
 	then
-		_mac_address_tag=$_p0
-	else
-		_p0=$(awk '{print toupper($1)}' /sys/class/ieee80211/phy1/macaddress 2> /dev/null)
 		_mac_address_tag=$_p0
 	fi
 	echo "$_mac_address_tag"
