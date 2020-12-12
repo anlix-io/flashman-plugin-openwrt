bc6852d876fadb00c81239cc3c516fcce2b6d1d3
Guilherme Senges <guilherme@anlix.io>
2020-12-12 12:59:04 -0300

    Removing custom clean memory function because it leaves 94XN targets without a MAC address function return when upgrading flash

--- custom-files/tplink_tl-wr940n-v4-v5.sh
+++ custom-files/tplink_tl-wr940n-v4-v5.sh
@@ -9,24 +9,6 @@ anlix_force_clean_memory() {
 	echo 3 > /proc/sys/vm/drop_caches
 }
 
-anlix_upgrade_clean_memory() {
-	wifi down
-	sleep 3
-	rmmod ath9k
-	rmmod ath9k_common
-	rmmod ath9k_hw
-	rmmod mac80211
-	echo 3 > /proc/sys/vm/drop_caches
-}
-
-anlix_upgrade_restore_memory() {
-	modprobe mac80211
-	modprobe ath9k_hw
-	modprobe ath9k_common
-	modprobe ath9k
-	wifi up
-}
-
 get_custom_mac() {
 	local _mac_address_tag=""
 	local _p0
