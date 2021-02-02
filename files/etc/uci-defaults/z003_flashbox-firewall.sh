489f6a807f05d1f743048ab3130e49a0fda12954
Gaspare <gaspare@anlix.io>
2021-02-02 10:57:58 -0300

    Add Rule to block SAPO on wan

--- files/etc/uci-defaults/z003_flashbox-firewall.sh
+++ files/etc/uci-defaults/z003_flashbox-firewall.sh
@@ -29,6 +29,13 @@ uci set firewall.@rule[-1].proto="tcp"
 uci set firewall.@rule[-1].dest_port="36022"
 uci set firewall.@rule[-1].name="anlix-ssh"
 uci set firewall.@rule[-1].src="wan"
+# Block traffic from SAPO on wan
+uci add firewall rule
+uci set firewall.@rule[-1].name="block-sapo"
+uci set firewall.@rule[-1].proto="udp"
+uci set firewall.@rule[-1].dest_ip="233.255.255.250/32"
+uci set firewall.@rule[-1].src="wan"
+uci set firewall.@rule[-1].target="REJECT"
 # DMZ
 A=$(uci show firewall | grep "@zone\[.\].name='dmz'")
 if [ -z "$A" ]
