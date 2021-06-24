c4e50889f857b5534c61a1e3a110146043faca09
Gaspare Bruno <gaspare@anlix.io>
2021-06-23 22:13:23 -0300

    Add Calibration File for C20

--- custom-files/tplink_archer-c20-v5.sh
+++ custom-files/tplink_archer-c20-v5.sh
@@ -7,3 +7,9 @@ get_custom_hardware_model() {
 get_custom_hardware_version() {
 	echo "V5"
 }
+
+wireless_firmware() {
+  #Firmware files - Clean this in the future (use firmware api in driver)
+  [ ! -e /lib/firmware/MT7628_EEPROM.bin ] && dd if=/dev/mtd9ro of=/lib/firmware/MT7628_EEPROM.bin bs=1 count=512
+  [ ! -e /lib/firmware/MT7610_EEPROM.bin ] && dd if=/dev/mtd9ro of=/lib/firmware/MT7610_EEPROM.bin bs=1k skip=32 count=32
+}
