07dec01571a533b0a10a098f0a62ba2ce632ff79
Gaspare Bruno <gaspare@anlix.io>
2021-08-18 11:11:29 -0300

    Add firmware file for Archer C50 v3/v4

--- custom-files/tplink_archer-c50-v3.sh
+++ custom-files/tplink_archer-c50-v3.sh
@@ -7,3 +7,9 @@ get_custom_hardware_model() {
 get_custom_hardware_version() {
 	echo "V3"
 }
+
+wireless_firmware() {
+  #Firmware files - Clean this in the future (use firmware api in driver)
+  [ ! -e /lib/firmware/MT7628_EEPROM.bin ] && dd if=/dev/mtd6ro of=/lib/firmware/MT7628_EEPROM.bin bs=1 skip=128k count=512
+  [ ! -e /lib/firmware/MT7612E_EEPROM.bin ] && dd if=/dev/mtd6ro of=/lib/firmware/MT7612E_EEPROM.bin bs=1 skip=160k count=32k
+}
--- custom-files/tplink_archer-c50-v4.sh
+++ custom-files/tplink_archer-c50-v4.sh
@@ -7,3 +7,9 @@ get_custom_hardware_model() {
 get_custom_hardware_version() {
 	echo "V4"
 }
+
+wireless_firmware() {
+  #Firmware files - Clean this in the future (use firmware api in driver)
+  [ ! -e /lib/firmware/MT7628_EEPROM.bin ] && dd if=/dev/mtd9ro of=/lib/firmware/MT7628_EEPROM.bin bs=1 count=512
+  [ ! -e /lib/firmware/MT7612E_EEPROM.bin ] && dd if=/dev/mtd9ro of=/lib/firmware/MT7612E_EEPROM.bin bs=1k skip=32 count=32
+}
