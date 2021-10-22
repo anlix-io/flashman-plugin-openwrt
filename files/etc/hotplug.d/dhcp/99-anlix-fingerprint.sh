9867cc82fc37f0b64f6bed946fb0d4bf77bf82df
Gaspare Bruno <gaspare@anlix.io>
2021-10-22 16:25:37 -0300

    Optimize DHCP finguerprint script

--- files/etc/hotplug.d/dhcp/99-anlix-fingerprint.sh
+++ files/etc/hotplug.d/dhcp/99-anlix-fingerprint.sh
@@ -1,14 +1,13 @@
 #!/bin/sh
 
-if [ ! -e /tmp/dhcpinfo ]
-then
-  mkdir /tmp/dhcpinfo
-fi
-
-if [ "$ACTION" = add ] || [ "$ACTION" = update ]
+if [ "$ACTION" = add ]
 then
   if [ -n "$DNSMASQ_REQUESTED_OPTIONS" ] && [ -n "$MACADDR" ]
   then
+    if [ ! -e /tmp/dhcpinfo ]
+    then
+      mkdir /tmp/dhcpinfo
+    fi
     echo "$DNSMASQ_REQUESTED_OPTIONS $DNSMASQ_VENDOR_CLASS" > /tmp/dhcpinfo/$MACADDR
   fi
 elif [ "$ACTION" = remove ]
