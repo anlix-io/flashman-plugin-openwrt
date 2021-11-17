241eb4ddd192f3207d23253b82420d3721bc9457
Romeu I. L. Pires <romeu.pires@anlix.io>
2021-11-17 16:12:05 -0300

    custom txpower handling in qca driver

--- driver/qcawifi.sh
+++ driver/qcawifi.sh
@@ -11,6 +11,12 @@ get_phy_type() {
 	[ "$1" == "ath0" ] && echo "1" || echo "2"
 }
 
+# Since it will be the default_radioX ifname, it still uses the logical device
+get_root_ifname() {
+	
+	[ "$1" == "0" ] && echo "ath0" || echo "ath1"
+}
+
 get_24ghz_phy() {
 	local netname
 	for i in /sys/class/net/* 
@@ -34,13 +40,38 @@ is_5ghz_vht() {
 	[ "$_5iface" ] && echo "1"
 }
 
-#get_wifi_htmode() {
-#
-#}
-#convert_txpower() {
-#
-#}
-#get_txpower() {
-#
-#}
+get_wifi_htmode(){
+	iwpriv ath$1 get_mode | sed -r -e 's/.*HT//g' -e 's/([0-9]+).*/\1/'
+}
+
+convert_txpower() {
+	local _freq="$1"
+	local _channel="$2"
+	local _target_tx_percent="$3"
+	local _maxpwr
+	
+	if [ $_freq = "24" ] ; then 
+		_maxpwr=$(custom_wifi_24_txpower)
+	elif [ $_freq = "50" ] ; then 
+		_maxpwr=$(custom_wifi_50_txpower)	
+	fi
+	echo $(( (_maxpwr*_target_tx_percent)/100 ))
+}
+
+get_txpower() {
+	local _idx="$1"
+	local _txpower="$(uci -q get wireless.radio$_idx.txpower)"
+	local _maxpwr
+
+	if   [ $_idx = "0" ]; then _maxpwr=$(custom_wifi_24_txpower)
+	elif [ $_idx = "1" ]; then _maxpwr=$(custom_wifi_50_txpower)	
+	fi
+
+	local _txprct="$(( (_txpower * 100) / _maxpwr ))"
+	if   [ $_txprct -ge 100 ]; then echo "100"
+	elif [ $_txprct -ge 75 ]; then echo "75"
+	elif [ $_txprct -ge 50 ]; then echo "50"
+	else echo "25"
+	fi
+}
 
