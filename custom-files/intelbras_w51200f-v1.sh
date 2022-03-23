41a0c1ef25cc660825e7f59f19b82bcb09da7bc6
lucasperovani <lucasperovani@gmail.com>
2022-03-23 16:32:36 -0300

    Added new parameters. Splitted code.

--- custom-files/intelbras_w51200f-v1.sh
+++ custom-files/intelbras_w51200f-v1.sh
@@ -5,9 +5,12 @@ anlix_bootup_defaults() {
 	# Get all parameters needed
 	# interface 1: 5G
 	# interface 2: 2.4G
-	local _parameters="$(anlix-flash-utils \
+	local _parameters_24g="$(anlix-flash-utils \
+2 reg_domain \
 2 x_cap \
 2 thermal \
+2 tr_switch \
+2 pa_type \
 2 power_level_cck_a \
 2 power_level_cck_b \
 2 power_level_ht40_1s_a \
@@ -17,46 +20,64 @@ anlix_bootup_defaults() {
 2 power_diff_ofdm \
 2 tx_power_tssi_ht40_1s_a \
 2 tx_power_tssi_ht40_1s_b \
+2 tssi_enable)"
+
+	local _parameters_5g="$(anlix-flash-utils \
+1 reg_domain \
 1 x_cap \
 1 thermal \
+1 tr_switch \
+1 pa_type \
 1 tx_power_5g_ht40_1s_a \
 1 tx_power_5g_ht40_1s_b \
 1 tx_power_diff_5g_ofdm \
 1 tx_power_diff_5g_80bw1s_160bw1s_a \
 1 tx_power_diff_5g_80bw1s_160bw1s_b \
 1 tx_power_tssi_5g_ht40_1s_a \
-1 tx_power_tssi_5g_ht40_1s_b)"
+1 tx_power_tssi_5g_ht40_1s_b \
+1 tssi_enable)"
 
-	if [ ! -z "$_parameters" ] && [ -n "$(echo $_parameters | grep '[ ERROR ]')" ]
+	if [ ! -z "$_parameters_24g" ] && [ ! -z "$_parameters_5g" ] && 
+	[ -n "$(echo $_parameters_24g | grep '[ ERROR ]')" ] && 
+	[ -n "$(echo $_parameters_5g | grep '[ ERROR ]')" ]
 	then
 
-		get_data 20 _parameter $_parameters
+		get_data 15 _parameter_24g $_parameters_24g
+		get_data 13 _parameter_5g $_parameters_5g
 
 		ifconfig wlan0 up
-		iwpriv wlan0 set_mib xcap=$_parameter0
-		iwpriv wlan0 set_mib ther=$_parameter1
-		iwpriv wlan0 set_mib pwrlevelCCK_A=$_parameter2
-		iwpriv wlan0 set_mib pwrlevelCCK_B=$_parameter3
-		iwpriv wlan0 set_mib pwrlevelHT40_1S_A=$_parameter4
-		iwpriv wlan0 set_mib pwrlevelHT40_1S_B=$_parameter5
-		iwpriv wlan0 set_mib pwrdiffHT40_2S=$_parameter6
-		iwpriv wlan0 set_mib pwrdiffHT20=$_parameter7
-		iwpriv wlan0 set_mib pwrdiffOFDM=$_parameter8
-		iwpriv wlan0 set_mib pwrlevel_TSSIHT40_1S_A=$_parameter9
-		iwpriv wlan0 set_mib pwrlevel_TSSIHT40_1S_B=$_parameter10
+		iwpriv wlan0 set_mib regdomain=$_parameter_24g0
+		iwpriv wlan0 set_mib xcap=$_parameter_24g1
+		iwpriv wlan0 set_mib ther=$_parameter_24g2
+		iwpriv wlan0 set_mib trswitch=$_parameter_24g3
+		iwpriv wlan0 set_mib pa_type=$_parameter_24g4
+		iwpriv wlan0 set_mib pwrlevelCCK_A=$_parameter_24g5
+		iwpriv wlan0 set_mib pwrlevelCCK_B=$_parameter_24g6
+		iwpriv wlan0 set_mib pwrlevelHT40_1S_A=$_parameter_24g7
+		iwpriv wlan0 set_mib pwrlevelHT40_1S_B=$_parameter_24g8
+		iwpriv wlan0 set_mib pwrdiffHT40_2S=$_parameter_24g9
+		iwpriv wlan0 set_mib pwrdiffHT20=$_parameter_24g10
+		iwpriv wlan0 set_mib pwrdiffOFDM=$_parameter_24g11
+		iwpriv wlan0 set_mib pwrlevel_TSSIHT40_1S_A=$_parameter_24g12
+		iwpriv wlan0 set_mib pwrlevel_TSSIHT40_1S_B=$_parameter_24g13
+		iwpriv wlan0 set_mib tssi_enable=$_parameter_24g14
 		ifconfig wlan0 down
 
 		ifconfig wlan1 up
-		iwpriv wlan0 set_mib txbf=1
-		iwpriv wlan1 set_mib xcap=$_parameter11
-		iwpriv wlan1 set_mib ther=$_parameter12
-		iwpriv wlan1 set_mib pwrlevel5GHT40_1S_A=$_parameter13
-		iwpriv wlan1 set_mib pwrlevel5GHT40_1S_B=$_parameter14
-		iwpriv wlan1 set_mib pwrdiff5GOFDM=$_parameter15
-		iwpriv wlan1 set_mib pwrdiff_5G_80BW1S_160BW1S_A=$_parameter16
-		iwpriv wlan1 set_mib pwrdiff_5G_80BW1S_160BW1S_B=$_parameter17
-		iwpriv wlan1 set_mib pwrlevel_TSSI5GHT40_1S_A=$_parameter18
-		iwpriv wlan1 set_mib pwrlevel_TSSI5GHT40_1S_B=$_parameter19
+		iwpriv wlan1 set_mib txbf=1
+		iwpriv wlan1 set_mib regdomain=$_parameter_5g0
+		iwpriv wlan1 set_mib xcap=$_parameter_5g1
+		iwpriv wlan1 set_mib ther=$_parameter_5g2
+		iwpriv wlan1 set_mib trswitch=$_parameter_5g3
+		iwpriv wlan1 set_mib pa_type=$_parameter_5g4
+		iwpriv wlan1 set_mib pwrlevel5GHT40_1S_A=$_parameter_5g5
+		iwpriv wlan1 set_mib pwrlevel5GHT40_1S_B=$_parameter_5g6
+		iwpriv wlan1 set_mib pwrdiff5GOFDM=$_parameter_5g7
+		iwpriv wlan1 set_mib pwrdiff_5G_80BW1S_160BW1S_A=$_parameter_5g8
+		iwpriv wlan1 set_mib pwrdiff_5G_80BW1S_160BW1S_B=$_parameter_5g9
+		iwpriv wlan1 set_mib pwrlevel_TSSI5GHT40_1S_A=$_parameter_5g10
+		iwpriv wlan1 set_mib pwrlevel_TSSI5GHT40_1S_B=$_parameter_5g11
+		iwpriv wlan1 set_mib tssi_enable=$_parameter_5g12
 		ifconfig wlan1 down
 
 	else
@@ -76,7 +97,7 @@ anlix_bootup_defaults() {
 		ifconfig wlan0 down
 
 		ifconfig wlan1 up
-		iwpriv wlan0 set_mib txbf=1
+		iwpriv wlan1 set_mib txbf=1
 		iwpriv wlan1 set_mib xcap=65
 		iwpriv wlan1 set_mib ther=33
 		iwpriv wlan1 set_mib pwrlevel5GHT40_1S_A=00000000000000000000000000000000000000000000000000000000000000000000005757575757555555555555555555555555555555555555555555555555505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505050505054545454545454545454545454545454545454545454545454545454545454545400000000000000000000000000000000000000
